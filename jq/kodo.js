a21f07ef1920ec8777e64c9e715457431a147b51
hjy <1216426312@qq.com>
2017-11-25 15:59:10 +0800

    知乎动态粒子背景

--- jq/kodo.js
+++ jq/kodo.js
@@ -327,6 +327,16 @@ var esDO = {
         }
         return _rs
     },
+    getUrl1: function(url) {
+        var url = url ? url : window.location.href,
+            search = url.substring(url.lastIndexOf("?") + 1)
+        if (!search) {
+            return {}
+        }
+        return JSON.parse('{"' + decodeURIComponent(search)
+            .replace(/"/g, '/\\"/')
+            .replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+    },
     //格式化字符串
     formatText: function(str, size, delimiter) {
         var _size = size || 3,
@@ -337,13 +347,34 @@ var esDO = {
     },
     //现金转大写
     upDigit: function(n) {
-        var fraction = ['角', '分', '厘'],
-            digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'],
-            unit = [
-                ['元', '万', '亿'],
-                ['', '拾', '佰', '仟']
-            ],
-            head = n > 0 ? '人民币' : ''
+        var fraction = ['角', '分'];
+        var digit = [
+            '零', '壹', '贰', '叁', '肆',
+            '伍', '陆', '柒', '捌', '玖'
+        ];
+        var unit = [
+            ['元', '万', '亿'],
+            ['', '拾', '佰', '仟']
+        ];
+        var head = n < 0 ? '欠' : '';
+        n = Math.abs(n);
+        var s = '';
+        for (var i = 0; i < fraction.length; i++) {
+            s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
+        }
+        s = s || '整';
+        n = Math.floor(n);
+        for (var i = 0; i < unit[0].length && n > 0; i++) {
+            var p = '';
+            for (var j = 0; j < unit[1].length && n > 0; j++) {
+                p = digit[n % 10] + unit[1][j] + p;
+                n = Math.floor(n / 10);
+            }
+            s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
+        }
+        return head + s.replace(/(零.)*零元/, '元')
+            .replace(/(零.)+/g, '零')
+            .replace(/^整$/, '零元整');
     },
     //清除对象值为空
     filterParam: function(obj) {
@@ -586,5 +617,6 @@ var esDO = {
             default:
                 return navigator.userAgent.toLowerCase()
         }
-    }
+    },
+
 };
